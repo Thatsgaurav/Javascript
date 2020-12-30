4f456f435e46154de09d67d1c2399fe0990ac105
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2020-12-30 19:26:04 +0530

    commit

---
+++ assets/scripts/assignment.js
@@ -0,0 +1,20 @@
+/*
+Assignment - Variables, Operators, Data Types
+Create two variables: One that holds a fictional user input (a number of your choice) and one that holds no value initially (e.g. "result").
+Set the result variable to 18 plus the value stored in user input.
+Add three additional lines of code where you change the result variable again (by subtracting a value of your choice, multiplying it and dividing it).
+Think about the value stored in the user input variable you also created - did that value change?
+alert() the result and the user input variables (in two separate alert() calls).
+
+const userInput = 20;
+let result;
+
+result = 18 + userInput;
+
+result = result - 10;
+result = result * 2;
+result = result / 4;
+
+alert(result);
+alert(userInput);
+*/
