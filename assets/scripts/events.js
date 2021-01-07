00dfec933c726073440c244c1569dafd663a811e
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-01-07 16:24:45 +0530

    commit

---
+++ assets/scripts/events.js
@@ -0,0 +1,20 @@
+const button = document.querySelector('button');
+
+//button.onclick = function() {
+
+//};
+
+// const buttonClickHandler = () => {
+//     alert('Button!')
+// };
+
+// const anotherButtonClickHandler = () => {
+//     console.log('This was another clicker!');
+// };
+
+// button.onclick = buttonClickHandler;
+// button.onclick = anotherButtonClickHandler;
+
+button.addEventListener();
+
+button.removeEventListener();
